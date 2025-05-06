bb154f7dce2ee50faaa9cd398fc6e4cdc826c36e
pedrodnsilveira <pedro.dns@gmail.com>
2025-05-05 21:32:50 -0300

    atualização autofarm

--- autoFarmAssistente.js
+++ autoFarmAssistente.js
@@ -1,3 +1,11 @@
+const urlAtual = window.location.href;
+
+if (!(urlAtual.includes("screen=overview_villages"))) {
+    alert("O script deve ser rodado na tela de visualização de aldeias");
+    return;
+}
+
+
 // Seleciona todos os spans com a classe 'quickedit-content'
 const spans = document.querySelectorAll('.quickedit-content');
 
@@ -17,27 +25,35 @@ spans.forEach(span => {
 });
 
 //console.log(villageIds); // Exibe um array com todos os IDs de village encontrados
-alert("IDs das aldeias:\n" + villageIds.join(", "));
-var url = "https://br134.tribalwars.com.br/game.php?village="+villageIds[0]+"&screen=am_farm";
-window.location.href = url;
+//alert("IDs das aldeias:\n" + villageIds.join(", "));
+for (let item of villageIds) {
+    var url = "https://br134.tribalwars.com.br/game.php?village="+item+"&screen=am_farm";
+    window.location.href = url;
 
+    let elemento = document.querySelector('a[class$="farm_icon farm_icon_b"]');
 
-(function () {
-    if (confirm("Começar o envio?")) {
-        function realizarAcao() {
-            let linkB = document.querySelector('a[class$="farm_icon farm_icon_b"]');
-            if (linkB) {
-                linkB.click();
-            } else {
-                console.error("Elemento não encontrado:", linkB);
-            }
-        }
-
-        var numeroAleatorio2 = Math.floor(Math.random() * (850 - 300)) + 300;
-
-        setTimeout(function () {
-            realizarAcao();
-            setInterval(realizarAcao, numeroAleatorio2);
-        }, numeroAleatorio2);
+    if (elemento && elemento.classList.contains('start_locked')) {
+        continue;
+    } else {
+        (function () {
+            //if (confirm("Começar o envio?")) {
+            /*function realizarAcao() {
+                //let linkB = document.querySelector('a[class$="farm_icon farm_icon_b"]');
+                if (elemento) {
+                    elemento.click();
+                } else {
+                    console.error("Elemento não encontrado:", elemento);
+                }
+            }*/
+            elemento.click();
+    
+            var tempo = Math.floor(Math.random() * (850 - 300)) + 300;
+    
+            setTimeout(function () {
+                realizarAcao();
+                setInterval(realizarAcao, tempo);
+            }, tempo);
+            //}
+        })();
     }
-})();
+}
