150a6a2f4830945d8b9abe1fd7aa74af499fb50c
pedrodnsilveira <pedro.dns@gmail.com>
2025-05-05 21:46:34 -0300

    atualização autofarm

--- autoFarmAssistente.js
+++ autoFarmAssistente.js
@@ -1,5 +1,5 @@
 const urlAtual = window.location.href;
-
+alert(urlAtual);
 if (!(urlAtual.includes("screen=overview_villages"))) {
     alert("O script deve ser rodado na tela de visualização de aldeias");
     return;
