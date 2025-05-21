6e788225efb65335863718402a6044c2957c829c
pedrodnsilveira <pedro.dns@gmail.com>
2025-05-21 12:28:47 -0300

    inclusao novo script resourcesTotal

---
+++ resourcesTotal.js
@@ -0,0 +1,25 @@
+/*TRATAMENTOS DE ERRO DE EXECUÇÃO*/
+if (!window.location.href.includes("screen=overview_villages&mode=prod")) {
+    alert("O script precisa ser executado na página Visualizações -> Produção");
+    throw new Error("Script interrompido. Página incorreta.");
+}
+
+function getSumResource(resourceType) {
+    const elements = document.querySelectorAll('.res.'+resourceType);
+    let total = 0;
+
+    elements.forEach(element => {
+        const value = parseInt(element.textContent.replace(/\D/g, ''), 10);
+        if (!isNaN(value)) {
+            total += value;
+        }
+    });
+
+    return total;
+}
+
+const wood = getSumResource('wood');
+const stone = getSumResource('stone');
+const iron = getSumResource('iron');
+const msg = `Madeira: ${wood} | Argila: ${stone} | Ferro: ${iron}`;
+alert(msg);
